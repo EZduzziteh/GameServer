e035ca4fec719c80f53b2ca95d7ee1ebb2abcc38
Sasha Greene <sashagreene@hotmail.com>
2024-06-21 13:18:27 -0600

    Initial commit

---
+++ GameServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = ("Game Server");
+            Server.Start(12, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
